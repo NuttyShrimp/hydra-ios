c6b28d34ff83cc2ff4e94d301aa23babd6ac3628
NuttyShrimp <jan.lecoutere@hotmail.be>
2024-10-07 12:33:07 +0200

    Initial Commit

---
+++ hydra-iOS/hydra_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  hydra_iOSApp.swift
+//  hydra-iOS
+//
+//  Created by Jan Lecoutere on 07/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct hydra_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
