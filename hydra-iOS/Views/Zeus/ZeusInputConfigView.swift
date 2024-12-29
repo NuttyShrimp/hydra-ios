37e62b799f20d3b271f2b92940aa756871476eb5
NuttyShrimp <jan.lecoutere@hotmail.be>
2024-12-29 13:29:51 +0100

    feat: add zeus config values to settings

---
+++ hydra-iOS/Views/Zeus/ZeusInputConfigView.swift
@@ -0,0 +1,58 @@
+//
+//  ZeusInputConfigView.swift
+//  hydra-iOS
+//
+//  Created by Jan Lecoutere on 28/12/2024.
+//
+
+import SwiftUI
+
+struct ZeusInputConfigView: View {
+    @AppStorage(GlobalConstants.StorageKeys.Zeus.username) var zeusUsername = ""
+    @AppStorage(GlobalConstants.StorageKeys.Zeus.tab) var zeusTabApiKey = ""
+    @AppStorage(GlobalConstants.StorageKeys.Zeus.tap) var zeusTapApiKey = ""
+    @AppStorage(GlobalConstants.StorageKeys.Zeus.door) var zeusDoorAccessApiKey = ""
+
+    var body: some View {
+        Form {
+            Section("Username") {
+                TextField(
+                    "Required",
+                    text: $zeusUsername
+                )
+                .textInputAutocapitalization(.never)
+            }
+
+            Section("API Key voor Tab") {
+                TextField(
+                    "Required",
+                    text: $zeusTabApiKey
+                )
+                .textInputAutocapitalization(.never)
+                .autocorrectionDisabled()
+            }
+
+            Section("API Key voor Tap") {
+                TextField(
+                    "Required",
+                    text: $zeusTapApiKey
+                )
+                .textInputAutocapitalization(.never)
+                .autocorrectionDisabled()
+            }
+
+            Section("API Key voor door access") {
+                TextField(
+                    "Optional",
+                    text: $zeusDoorAccessApiKey
+                )
+                .textInputAutocapitalization(.never)
+                .autocorrectionDisabled()
+            }
+        }
+    }
+}
+
+#Preview {
+    ZeusInputConfigView()
+}
--- hydra-iOS/Views/Zeus/ZeusOnboardingView.swift
+++ hydra-iOS/Views/Zeus/ZeusOnboardingView.swift
@@ -9,49 +9,10 @@ import SwiftUI
 
 struct ZeusOnboardingView: View {
     @AppStorage(GlobalConstants.StorageKeys.Zeus.onboarding) var onboardingFinished = false
-    @AppStorage(GlobalConstants.StorageKeys.Zeus.username) var zeusUsername = ""
-    @AppStorage(GlobalConstants.StorageKeys.Zeus.tab) var zeusTabApiKey = ""
-    @AppStorage(GlobalConstants.StorageKeys.Zeus.tap) var zeusTapApiKey = ""
-    @AppStorage(GlobalConstants.StorageKeys.Zeus.door) var zeusDoorAccessApiKey = ""
 
     var body: some View {
         VStack {
-            Form {
-                Section("Username") {
-                    TextField(
-                        "Required",
-                        text: $zeusUsername
-                    )
-                    .textInputAutocapitalization(.never)
-                }
-
-                Section("API Key voor Tab") {
-                    TextField(
-                        "Required",
-                        text: $zeusTabApiKey
-                    )
-                    .textInputAutocapitalization(.never)
-                    .autocorrectionDisabled()
-                }
-
-                Section("API Key voor Tap") {
-                    TextField(
-                        "Required",
-                        text: $zeusTapApiKey
-                    )
-                    .textInputAutocapitalization(.never)
-                    .autocorrectionDisabled()
-                }
-
-                Section("API Key voor door access") {
-                    TextField(
-                        "Optional",
-                        text: $zeusDoorAccessApiKey
-                    )
-                    .textInputAutocapitalization(.never)
-                    .autocorrectionDisabled()
-                }
-            }
+            ZeusInputConfigView()
             Button(
                 action: {
                     onboardingFinished = true
