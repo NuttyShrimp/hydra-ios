4a7920751f56cbe83727a5878907cf669352144a
NuttyShrimp <jan.lecoutere@hotmail.be>
2025-03-02 01:41:14 +0100

    fix(zeus): input for keys decided to break again

--- hydra-iOS/Views/Zeus/ZeusInputConfigView.swift
+++ hydra-iOS/Views/Zeus/ZeusInputConfigView.swift
@@ -8,22 +8,11 @@
 import SwiftUI
 
 struct ZeusInputConfigView: View {
-    @State var zeusUsername = ZeusConfig.sharedInstance.username ?? "" {
-        didSet {
-            ZeusConfig.sharedInstance.username = zeusUsername
-        }
-    }
-    @State var zeusTabApiKey = ZeusConfig.sharedInstance.tabToken ?? "" {
-        didSet {
-            ZeusConfig.sharedInstance.tabToken = zeusTabApiKey
-        }
-    }
-    @State var zeusDoorAccessApiKey = ZeusConfig.sharedInstance.doorToken ?? "" {
-        didSet {
-            ZeusConfig.sharedInstance.doorToken = zeusDoorAccessApiKey
-        }
-    }
-
+    @State var zeusUsername = ZeusConfig.sharedInstance.username ?? ""
+    @State var zeusTabApiKey = ZeusConfig.sharedInstance.tabToken ?? ""
+    @State var zeusTapApiKey = ZeusConfig.sharedInstance.tapToken ?? ""
+    @State var zeusDoorAccessApiKey = ZeusConfig.sharedInstance.doorToken ?? ""
+    
     var body: some View {
         Form {
             Section("Username") {
@@ -31,6 +20,9 @@ struct ZeusInputConfigView: View {
                     "Required",
                     text: $zeusUsername
                 )
+                .onChange(of: zeusUsername) { newValue in
+                    ZeusConfig.sharedInstance.username = newValue
+                }
                 .textInputAutocapitalization(.never)
             }
 
@@ -39,15 +31,33 @@ struct ZeusInputConfigView: View {
                     "Required",
                     text: $zeusTabApiKey
                 )
+                .onChange(of: zeusTabApiKey) { newValue in
+                    ZeusConfig.sharedInstance.tabToken = newValue
+                }
                 .textInputAutocapitalization(.never)
                 .autocorrectionDisabled()
             }
-
+            
+            Section("API Key voor Tap") {
+                TextField(
+                    "Required",
+                    text: $zeusTapApiKey
+                )
+                .onChange(of: zeusTapApiKey) { newValue in
+                    ZeusConfig.sharedInstance.tapToken = newValue
+                }
+                .textInputAutocapitalization(.never)
+                .autocorrectionDisabled()
+            }
+            
             Section("API Key voor door access") {
                 TextField(
                     "Optional",
                     text: $zeusDoorAccessApiKey
                 )
+                .onChange(of: zeusDoorAccessApiKey) { newValue in
+                    ZeusConfig.sharedInstance.doorToken = newValue
+                }
                 .textInputAutocapitalization(.never)
                 .autocorrectionDisabled()
             }
