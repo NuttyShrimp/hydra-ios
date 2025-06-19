041f510712e53c1d7d8e20fe492b34a3d3a776c5
NuttyShrimp <jan.lecoutere@hotmail.be>
2025-06-19 15:23:00 +0200

    feat(zeusConfig): load keys from plist when available for easier dx

---
+++ hydra-iOS/Models/APIKeysPreferences.swift
@@ -0,0 +1,13 @@
+//
+//  APIKeysPreferences.swift
+//  hydra-iOS
+//
+//  Created by Jan Lecoutere on 19/06/2025.
+//
+
+struct APIKeysPreferences: Codable {
+    var ZEUS_USERNAME: String?
+    var TAB_KEY: String?
+    var TAP_KEY: String?
+    var MM_KEY: String?
+}
--- hydra-iOS/Models/Zeus/ZeusConfig.swift
+++ hydra-iOS/Models/Zeus/ZeusConfig.swift
@@ -11,7 +11,31 @@ typealias ZeusKeys = GlobalConstants.StorageKeys.Zeus
 
 struct ZeusConfig {
     @MainActor static var sharedInstance = ZeusConfig()
-
+    
+    private init() {
+        if let plistURL = Bundle.main.url(forResource: "APIKeys", withExtension: "plist") {
+            do {
+                let plistData = try Data(contentsOf: plistURL)
+                let keys = try PropertyListDecoder().decode(APIKeysPreferences.self, from: plistData)
+                if let username = keys.ZEUS_USERNAME {
+                    self.username = username
+                }
+                if let doorToken = keys.MM_KEY {
+                    self.doorToken = doorToken
+                }
+                if let tabToken = keys.TAB_KEY {
+                    self.tabToken = tabToken
+                }
+                if let tapToken = keys.TAP_KEY {
+                    self.tapToken = tapToken
+                }
+            } catch {
+                print("Failed to load API Keys, this is not critical")
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
     var username = UserDefaults.standard.string(
         forKey: ZeusKeys.username)
     {
