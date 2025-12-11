a62793fa67afad3160b6d7b0a4d6d9ad05a6f5a8
NuttyShrimp <jan.lecoutere@hotmail.be>
2025-12-11 18:52:02 +0100

    fix(resto): nav stack is picky about placement

--- hydra-iOS/Views/RestoView.swift
+++ hydra-iOS/Views/RestoView.swift
@@ -40,18 +40,6 @@ struct RestoView: View {
                     otherMenu
                 }
             }
-            .navigationDestination(for: RestoNavigationOptions.self) { option in
-                ZStack {
-                    switch option {
-                    case .extraMenus:
-                        OtherFoodMenuView(resto: restos)
-                    case .locations:
-                        RestoLocations(restos: restos)
-                    }
-                }
-                .navigationTitle(option.toString().capitalized)
-                .navigationBarTitleDisplayMode(.inline)
-            }
         }
         .navigationTitle("Resto's")
         .navigationBarTitleDisplayMode(.inline)
@@ -100,5 +88,17 @@ struct RestoView: View {
                 }
             }, label: { Image(systemName: "ellipsis") }
         )
+        .navigationDestination(for: RestoNavigationOptions.self) { option in
+            ZStack {
+                switch option {
+                case .extraMenus:
+                    OtherFoodMenuView(resto: restos)
+                case .locations:
+                    RestoLocations(restos: restos)
+                }
+            }
+            .navigationTitle(option.toString().capitalized)
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
